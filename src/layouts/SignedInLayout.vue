2339c23fa9f03f6edc2584c322b4237ef5c42394
TsuZAke26 <ricky.sanders62@gmail.com>
2024-09-16 14:16:27 -0500

    Semi-fixed navbar not being sticky (still need to center page content correctly)

--- src/layouts/SignedInLayout.vue
+++ src/layouts/SignedInLayout.vue
@@ -1,28 +1,30 @@
 <template>
-  <!-- App Navigation Bar -->
-  <header class="p-4">
-    <Menubar :model="items">
-      <template #item="{ item, props, hasSubmenu }">
-        <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
-          <a :href="href" v-bind="props.action" @click="navigate">
+  <div class="relative">
+    <!-- App Navigation Bar -->
+    <header class="sticky top-0 z-10 w-full">
+      <Menubar :model="items">
+        <template #item="{ item, props, hasSubmenu }">
+          <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+            <a :href="href" v-bind="props.action" @click="navigate">
+              <span :class="item.icon" />
+              <span class="ml-2">{{ item.label }}</span>
+            </a>
+          </router-link>
+          <a v-else :href="item.url" :target="item.target" v-bind="props.action">
             <span :class="item.icon" />
             <span class="ml-2">{{ item.label }}</span>
+            <span v-if="hasSubmenu" class="ml-2 pi pi-fw pi-angle-down" />
           </a>
-        </router-link>
-        <a v-else :href="item.url" :target="item.target" v-bind="props.action">
-          <span :class="item.icon" />
-          <span class="ml-2">{{ item.label }}</span>
-          <span v-if="hasSubmenu" class="ml-2 pi pi-fw pi-angle-down" />
-        </a>
-      </template>
-      <template #end><div class="font-semibold">Financial Tracker</div></template>
-    </Menubar>
-  </header>
+        </template>
+        <template #end><div class="font-semibold">Financial Tracker</div></template>
+      </Menubar>
+    </header>
 
-  <!-- Main Content -->
-  <main class="mx-4">
-    <RouterView />
-  </main>
+    <!-- Main Content -->
+    <main class="mx-4 mt-4">
+      <RouterView />
+    </main>
+  </div>
 </template>
 
 <script setup lang="ts">
